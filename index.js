c9936b0a7e91e0d2af0310f7debe82fd16fb0b2a
Mahedimm <mahedi15-12649@diu.edu.bd>
2021-10-30 19:38:40 +0600

    heroco

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const { ObjectId } = require('mongodb');
 
 const app = express();
-const port = process.env.Port || 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
